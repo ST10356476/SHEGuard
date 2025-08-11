0608ba428ed8c4d04d047bcf3b6db266fa96ca9d
Joashp01 <St10090834@vcconnect.edu.za>
2025-08-11 12:47:39 +0200

    Secure vaults pages basic frontend done

---
+++ app/src/main/java/com/iiest10356476/sheguard/SecureVaultViewAll.kt
@@ -0,0 +1,20 @@
+package com.iiest10356476.sheguard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecureVaultViewAll : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_secure_vault_view_all)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
